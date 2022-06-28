84129172c2a28d5d76002d269fdfdaf5b31bef28
Ariful <arifulbgt4@gmail.com>
2022-06-28 13:26:59 +0600

    feat: add linter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,21 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>ARIFUL</title>
-        <meta name="description" content="Ariful personal portfolio" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Home: NextPage = () => (
+  <div className={styles.container}>
+    <Head>
+      <title>ARIFUL</title>
+      <meta name="description" content="Ariful personal portfolio" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <main className={styles.main}>
-        <h3 className={styles.title}>
-          Welcome to <a href="https://ariful.io">Ariful's personal portfolio.</a>
-        </h3>
-      </main>
-    </div>
-  )
-}
+    <main className={styles.main}>
+      <h3 className={styles.title}>
+        Welcome to <a href="https://ariful.io">Ariful's personal portfolio.</a>
+      </h3>
+    </main>
+  </div>
+);
 
-export default Home
+export default Home;
