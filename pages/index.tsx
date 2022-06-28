6032bc9b91251d4346558c73427c18c894018433
Ariful <arifulbgt4@gmail.com>
2022-06-28 12:12:24 +0600

    refactor: linter error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -14,7 +13,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://ariful.io">Ariful's personal portfolio</a>
+          Welcome to <a href="https://ariful.io">Ariful's personal</a>
         </h1>
       </main>
     </div>
