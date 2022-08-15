193de65f0e2a47a1ab5b3f8b131e6f9e15a8687e
Ariful <arifulbgt4@gmail.com>
2022-08-15 22:52:45 +0600

    chore: refactor

--- components/common/Fetch/index.tsx
+++ components/common/Fetch/index.tsx
@@ -2,4 +2,5 @@ import { IFetch } from './Types';
 
 const Fetch = ({ children, loading, skeleton, empty, data }: IFetch) =>
   loading ? skeleton : data ? children(data) : empty;
+
 export default Fetch;
