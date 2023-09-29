a40fd70e56c76390cf8e7e3841f550de2bf39333
Ariful islam <arifulbgt4@gmail.com>
2023-09-29 22:56:29 +0600

    upgrade: seo meta contents

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,49 @@ import ThemeContextProvider from "src/theme";
 import DefaultLayout from "src/layouts/DefaultLayout";
 // Widgets
 import Footer from "src/widgets/Footer";
+import { siteConfig } from "src/global/config";
 
 export const metadata = {
-  title: "Ariful islam",
-  description:
-    "Ariful personal portfolio, Fullstack, Full-stack, Full stack, Fullstack developer",
   viewport: "initial-scale=1, width=device-width",
+  title: { default: siteConfig.name, template: `%s - ${siteConfig.name}` },
+  description: siteConfig.description,
+  keywords: siteConfig.keywords,
+  authors: [{ name: siteConfig.author, url: siteConfig.url }],
+  creator: siteConfig.creator,
+  manifest: `${siteConfig.url}manifest.json`,
+  metadataBase: new URL(siteConfig.url),
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@ArifulI60735491",
+  },
 };
 
 export default function RootLayout({
