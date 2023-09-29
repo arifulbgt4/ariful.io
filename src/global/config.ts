b355c90f415d4a42bcef20a741b976b22b967d07
Ariful islam <arifulbgt4@gmail.com>
2023-09-29 23:20:34 +0600

    fix: typo error

--- src/global/config.ts
+++ src/global/config.ts
@@ -3,7 +3,7 @@ const siteAddress = process.env.NEXT_PUBLIC_SITE_ADDRESS || "http://ariful.io/";
 export const siteConfig = {
   name: "Experienced Senior Frontend Engineer | JavaScript, React.js, Next.js Expert",
   url: siteAddress,
-  ogImage: `${siteAddress}/opengraph-image`,
+  ogImage: `${siteAddress}opengraph-image`,
   description:
     "Passionate senior frontend engineer specializing in JavaScript, React.js, Next.js, and Node.js. Transforming ideas into seamless, user-centric experiences. Let's build the future together.",
   links: {
